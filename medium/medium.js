66f77c9ac4cec9f583ea7da2601484d43f29c17a
agungcaproex <agung.caproex@gmail.com>
2018-03-09 11:01:28 +0700

    titip sbelum error

--- medium/medium.js
+++ medium/medium.js
@@ -1,18 +1,26 @@
 function generateBorderBox(input){
     let result = []
+    let mid = Math.floor(input/2)
 
     for(let i=0; i<input; i++){
         let inside = []
         for(let j=0; j<input; j++){
             
-            if(i == 0 || i == input-1){
+            if(i == 0 || i == input-1 || j == 0 || j == input-1){
                 inside.push('I')
             }
-            else if(j == 0 || j == input-1){
+            else if(i == 2 && j > 1 && j < input-2){
                 inside.push('I')
             }
-            else if(i == 2 || i == input-3){
+            else if(i == input-3 && j > 1 && j < input-2){
                 inside.push('I')
+            }
+            else if(j == 2 && i > 2 && i < input-2){
+                inside.push('I')
+            }
+            
+            else if(i == mid && j == mid){
+                inside.push('*')
             }
             else{
                 inside.push(' ')
