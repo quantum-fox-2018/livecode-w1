6c8944c4a91714d52421012cc6cc54ced330511f
Muhammad Ramadiansyah <muhammadramadiansyah@Muhammads-MacBook-Pro.local>
2018-03-09 11:32:28 +0700

    live code update

--- hard/soalHard.js
+++ hard/soalHard.js
@@ -1,9 +1,16 @@
 function generateBarChart(numbers){
 
   var batangSign = " III ";
-  let outputResult = ""
+  let outputResult = "";
+  let nilaiMax = numbers[0];
+  for(let i=0;i<numbers.length;i++){
 
-  for(let i=9;i>=0;i--){
+    if(nilaiMax<numbers[i]){
+      nilaiMax = numbers[i];
+    }
+  }
+
+  for(let i=nilaiMax;i>=0;i--){
     outputResult = "";
     outputResult = outputResult + i + '|'
 
@@ -26,5 +33,5 @@ function generateBarChart(numbers){
 }
 
 generateBarChart([1,2,3,4,5,4,3,2,1]);
-generateBarChart([3,6,4,7,2]);
-generateBarChart([9,8,7,0,1,2,3]);
+//generateBarChart([3,6,4,7,2]);
+//generateBarChart([9,8,7,0,1,2,3]);
